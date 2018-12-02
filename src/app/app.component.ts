92d2bdd61af460fc667c3466d80fc75184c193ec
saver711 <saver711>
2018-12-02 12:03:15 +0200

    ____

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,7 +10,7 @@ import { TabsPage } from '../pages/tabs/tabs';
 })
 export class MyApp {
 rootPage:any = TabsPage;
-    //rootPage:string = 'SettingsPage';
+    //rootPage:string = 'SettingsPage'; 
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
--- src/pages/settings/settings.ts
+++ src/pages/settings/settings.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
-
+declare var $:any;
 @IonicPage()
 @Component({
   selector: 'page-settings',
@@ -16,13 +16,31 @@ export class SettingsPage {
     console.log('ionViewDidLoad SettingsPage');
   }
     
-    goTo(target) {
-        if (target == 'editSettings') {
-            this.navCtrl.push('EditSettingsPage');
-        } else if (target == 'editPassword') {
-            this.navCtrl.push('EditPasswordPage');
+    goToeditPassword() {
+        this.navCtrl.push('EditPasswordPage');
+    }
+    
+    slideDown(target) {
+        if(target == 'editName') {
+            $('.set__edit--name').slideDown(200).css('display', 'flex');
+        } else if(target == 'editPhone') {
+            $('.set__edit--phone').slideDown(200).css('display', 'flex');
+        } else if(target == 'editEmail') {
+            $('.set__edit--email').slideDown(200).css('display', 'flex');
         } else {
-            console.log('something is wrong');
+            console.log('something is wrong')
+        }
+    }
+    
+    slideUp(target) {
+        if(target == 'editName') {
+            $('.set__edit--name').slideUp(200);
+        } else if(target == 'editPhone') {
+            $('.set__edit--phone').slideUp(200);
+        } else if(target == 'editEmail') {
+            $('.set__edit--email').slideUp(200);
+        } else {
+            console.log('something is wrong')
         }
     }
 
